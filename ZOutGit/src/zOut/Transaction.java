05c8de0bbdb88dc415eecdf2b7dab413ea6fa295
Harris Pittinsky <hpittin1@binghamton.edu>
2014-04-05 15:17:17 -0400

    update gitignore, add transaction object

---
+++ ZOutGit/src/zOut/Transaction.java
@@ -0,0 +1,37 @@
+package zOut;
+
+import java.io.Serializable;
+
+public class Transaction implements Serializable {
+
+	private static final long serialVersionUID = 7423120658539374238L;
+	private int numChecks, numCards;
+	private String date, checkSubTotal, cardSubTotal, cashSubTotal,
+			registerStartingAmt, subTotal, total;
+
+	public Transaction(String aDate, int numberChecks, String aCheckSubTotal,
+			int numberCards, String aCardSubTotal, String aCashSubTotal,
+			String registerStartingAmount, String aSubTotal, String theTotal) {
+		this.date = aDate;
+		this.numChecks = numberChecks;
+		this.checkSubTotal = aCheckSubTotal;
+		this.numCards = numberCards;
+		this.cardSubTotal = aCardSubTotal;
+		this.cashSubTotal = aCashSubTotal;
+		this.registerStartingAmt = registerStartingAmount;
+		this.subTotal = aSubTotal;
+		this.total = theTotal;
+	}
+@Override
+public String toString() {
+	String str = date + "\n" + "\n" + "Number of Checks: " + numChecks
+			+ "\n" + "Check Sub-Total: " + checkSubTotal + "\n" + "\n"
+			+ "Number of Credit Cards: " + numCards + "\n"
+			+ "Credit Sub-Total: " + cardSubTotal + "\n" + "\n"
+			+ "Cash Sub-Total: " + cashSubTotal + "\n" + "\n"
+			+ "Register Starting Amount: " + registerStartingAmt + "\n"
+			+ "Sub-Total: " + subTotal + "\n" + "Total: " + total + "\n"
+			+ "------------------------------------";
+	return str;
+}
+}
