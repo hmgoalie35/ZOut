82e0aa4b92f7176c63d5e48523b04e2caa08b4c6
Harris Pittinsky <hpittin1@binghamton.edu>
2014-04-18 16:18:56 -0400

    changed some class variables to be doubles instead of Strings, to allow for easy handling of numbers. Added variables for month, day, year of the transaction so it is easy to search for these attributes. Changed the getTotal method to return double. Added getMonthDayYear method. Changed toString so formatting of numbers with commas, $ only occurs in the toString

--- ZOutGit/src/zOut/Transaction.java
+++ ZOutGit/src/zOut/Transaction.java
@@ -1,17 +1,24 @@
 package zOut;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
 
 public class Transaction implements Serializable {
 
 	private static final long serialVersionUID = 7423120658539374238L;
-	private int numChecks, numCards;
-	private String date, checkSubTotal, cardSubTotal, cashSubTotal,
-			registerStartingAmt, subTotal, total;
+	private int numChecks, numCards, month, day, year;
+	private String date;
+	double checkSubTotal, cardSubTotal, cashSubTotal, registerStartingAmt,
+			subTotal, total;
+	private NumberFormat moneyFormat = NumberFormat.getCurrencyInstance();
 
-	public Transaction(String aDate, int numberChecks, String aCheckSubTotal,
-			int numberCards, String aCardSubTotal, String aCashSubTotal,
-			String registerStartingAmount, String aSubTotal, String theTotal) {
+	public Transaction(int aMonth, int aDay, int aYear, String aDate,
+			int numberChecks, double aCheckSubTotal, int numberCards,
+			double aCardSubTotal, double aCashSubTotal,
+			double registerStartingAmount, double aSubTotal, double theTotal) {
+		this.month = aMonth;
+		this.day = aDay;
+		this.year = aYear;
 		this.date = aDate;
 		this.numChecks = numberChecks;
 		this.checkSubTotal = aCheckSubTotal;
@@ -22,32 +29,50 @@ public class Transaction implements Serializable {
 		this.subTotal = aSubTotal;
 		this.total = theTotal;
 	}
-	
+
 	/**
 	 * Accessor for the date this transaction was create
+	 * 
 	 * @return the date of the transaction
 	 */
-	public String getDate(){
+	public String getDate() {
 		return date;
 	}
-	
+
 	/**
 	 * Accessor for the total of this transaction
 	 * @return the total of the transaction
 	 */
-	public String getTotal(){
+	public double getTotal() {
 		return total;
 	}
-@Override
-public String toString() {
-	String str = date + "\n" + "\n" + "Number of Checks: " + numChecks
-			+ "\n" + "Check Sub-Total: " + checkSubTotal + "\n" + "\n"
-			+ "Number of Credit Cards: " + numCards + "\n"
-			+ "Credit Sub-Total: " + cardSubTotal + "\n" + "\n"
-			+ "Cash Sub-Total: " + cashSubTotal + "\n" + "\n"
-			+ "Register Starting Amount: " + registerStartingAmt + "\n"
-			+ "Sub-Total: " + subTotal + "\n" + "Total: " + total + "\n"
-			+ "------------------------------------";
-	return str;
-}
+
+	public String getMonthDayYear() {
+		return month + "-" + day + "-" + year;
+	}
+
+	@Override
+	public String toString() {
+		String calculatedTotal = null;
+		if(total < 0){
+			String data = moneyFormat.format(total);
+			data = data.substring(2, data.length() - 1);
+		    calculatedTotal = "$-" + data;
+		}else{
+			calculatedTotal = moneyFormat.format(total);
+		}
+		String str = date + "\n" + "\n" + "Number of Checks: " + numChecks
+				+ "\n" + "Check Sub-Total: "
+				+ moneyFormat.format(checkSubTotal) + "\n" + "\n"
+				+ "Number of Credit Cards: " + numCards + "\n"
+				+ "Credit Sub-Total: " + moneyFormat.format(cardSubTotal)
+				+ "\n" + "\n" + "Cash Sub-Total: "
+				+ moneyFormat.format(cashSubTotal) + "\n" + "\n"
+				+ "Register Starting Amount: "
+				+ moneyFormat.format(registerStartingAmt) + "\n"
+				+ "Sub-Total: " + moneyFormat.format(subTotal) + "\n"
+				+ "Total: " + calculatedTotal + "\n"
+				+ "------------------------------------";
+		return str;
+	}
 }
